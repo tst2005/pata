45ff88615b186e01bb51475dca4c30ac0cdaa50b
TsT <tst2005@gmail.com>
2018-06-04 14:37:19 +0200

    jq/db: db_keepresult string argument

--- mods/jq/db_keepresult.lib.sh
+++ mods/jq/db_keepresult.lib.sh
@@ -1,2 +1,2 @@
 jq_function_db_keepresult='def db_keepresult($resultfield;$dbfield;$idfield):
-	(.[$resultfield]|map({key:., value:true})|from_entries) as $want | .[dbfield] |= map(select($want[.[$idfield]]))|del(.[resultfield]);'
+	(.[$resultfield]|map({key:., value:true})|from_entries) as $want | .[$dbfield] |= map(select($want[.[$idfield]]))|del(.[$resultfield]);'
